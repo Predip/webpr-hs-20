1c23d61c580c8ef599ea3ad302a7ce20ce5078cb
Pradip Ravichandran <piradip_94@hotmail.com>
2020-09-22 12:11:28 +0200

    week 1

--- week1/snake/snake.js
+++ week1/snake/snake.js
@@ -18,11 +18,12 @@ let snake = [
 let food = {x: 15, y: 15};
 
 function snakeEquals(a, b) { 
-	/* fill here */
+    return a.x == b.x && a.y == b.y;
 }
 
 function changeDirection(orientation) {
-    /* fill here */
+    const idx = orientation.indexOf(direction);
+    direction = orientation[idx + 1];
 }
 
 function start() {
@@ -63,9 +64,11 @@ function nextBoard() {
         food.y = Math.floor(Math.random() * 20);
     } else {
         /* fill here */ // no food found => no growth despite new head => remove last element
+        snake.pop()
     }
 
     /* fill here */; // put head at front of the list
+    snake.unshift(head);
 }
 
 function display(context) {
